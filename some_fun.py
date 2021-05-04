47df9a3aeb040ef3259ae1978cc728f0f00628c0
Dmitriy <aziraal007@yandex.ru>
2021-05-04 17:59:50 +0300

    change some_fun.py file

--- some_fun.py
+++ some_fun.py
@@ -10,3 +10,59 @@ def sum_num(a, b):
 
 print(f'Использование анонимной функции', reduce(lambda a, b: a + b, number))
 print(f'Использование кастомной функции', reduce(sum_num, number))
+
+print('-------------------------------------------------------------------------------')
+
+
+def hello(name: str) -> str:
+    return f'Hello {name}'
+
+
+def good_bye(name: str) -> str:
+    return f'Good bye {name}'
+
+
+def some_fun(func):
+    return func("Dima")
+
+
+print(some_fun(hello))
+print('-------------------------------------------------------------------------------')
+
+
+def parent_fun():
+    print('parent print')
+
+    """Определение функции не говорит о порядке ее вызова"""
+
+    def second_child():
+        print('second print')
+
+    def first_child():
+        print('first print')
+
+    """Порядок вызова определяется здесь"""
+    first_child()
+    second_child()
+
+
+parent_fun()
+
+"""Декоратор"""
+
+
+def some_decorator(func):
+    def wrapper():
+        print(f'Before call function')
+        func()
+        print(f'After call function')
+
+    return wrapper
+
+
+@some_decorator
+def call_fun():
+    print('Call func')
+
+
+call_fun()
